8f58c2a2b225998e4ab0de2fcd8961277a88450f
Brandon Nichols <brandonaronnichols@gamil.com>
2020-06-09 21:59:22 -0500

    finished single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,39 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# Understand
+## takes in a list of ints
+## list can be any size but needs at least one number that has no duplicate so minimum size of one
+## returns the integer that has no duplicate in the list
+## the output should be a single integer
+## can return negative and positive numbers
+## I'm not sure if this list of ints is in order
+# Plan
+## since I don't know if the list is in order I may want to sort it
+#### this will help me find the non duplicate number easier as it will be surrounded by duplicates
+## there might be a python library that helps me find non duplicate things in lists *shrugs*
+## maybe I can use regex on this list, but I would need to turn the values into strings first to use regex
+## google a way in python to find duplicate items in a list :)
+
+####################################################################
+
+# Counter solution
+# https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
+
+# import collections counter, can be used to turn a list into a dictionary with a count of unique items in it
+
+import collections
+
 def single_number(arr):
     # Your code here
 
-    pass
+    # loop through arr:
+        # if the touple that is obtain from calling Collections.Counter() on the arr count is one then we 
+            # return that number
+
+    for item, count in collections.Counter(arr).items():
+        if count == 1:
+            return item
 
 
 if __name__ == '__main__':
