2c1e75a0b113b27106bbfc0ad66fce6601d2b00b
Brandon Nichols <brandonaronnichols@gamil.com>
2020-06-11 21:46:26 -0500

    changed some of my tests in my main file

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -59,36 +59,36 @@ def eating_cookies(n, arr = None):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    # num_cookies = 0
+    num_cookies = 0
 
-    # print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 1
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 2
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 3
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 4
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 6
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 7
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 8
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 9
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
     num_cookies = 10
 
-    print(f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies)])} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
